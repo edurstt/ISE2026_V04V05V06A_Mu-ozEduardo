526489527d6eecfb022f438f6240c2c83b9b1758
Edu <edurstt@gmail.com>
2026-03-10 11:23:51 +0100

    P1 Subida Inicial y Final -- Comienzo de uso del git de ISE

---
+++ HTTP_Server/RTE/_Debug/RTE_Components.h
@@ -0,0 +1,59 @@
+
+/*
+ * Auto generated Run-Time-Environment Configuration File
+ *      *** Do not modify ! ***
+ *
+ * Project: 'HTTP_Server' 
+ * Target:  'Debug' 
+ */
+
+#ifndef RTE_COMPONENTS_H
+#define RTE_COMPONENTS_H
+
+
+/*
+ * Define the Device Header File: 
+ */
+#define CMSIS_device_header "stm32f4xx.h"
+
+/* Keil.ARM Compiler::Compiler:Event Recorder:DAP:1.4.0 */
+#define RTE_Compiler_EventRecorder
+          #define RTE_Compiler_EventRecorder_DAP
+/* Keil::CMSIS Driver:Ethernet MAC:2.9 */
+#define RTE_Drivers_ETH_MAC0            /* Driver ETH_MAC0 */
+/* Keil::CMSIS Driver:Ethernet PHY:LAN8742A:1.3.0 */
+#define RTE_Drivers_PHY_LAN8742A        /* Driver PHY LAN8742A */
+/* Keil::CMSIS Driver:SPI:2.15 */
+#define RTE_Drivers_SPI1                /* Driver SPI1 */
+        #define RTE_Drivers_SPI2                /* Driver SPI2 */
+        #define RTE_Drivers_SPI3                /* Driver SPI3 */
+        #define RTE_Drivers_SPI4                /* Driver SPI4 */
+        #define RTE_Drivers_SPI5                /* Driver SPI5 */
+        #define RTE_Drivers_SPI6                /* Driver SPI6 */
+/* Keil::Device:STM32Cube Framework:Classic:1.7.9 */
+#define RTE_DEVICE_FRAMEWORK_CLASSIC
+/* Keil::Device:STM32Cube HAL:ADC:1.7.9 */
+#define RTE_DEVICE_HAL_ADC
+/* Keil::Device:STM32Cube HAL:Common:1.7.9 */
+#define RTE_DEVICE_HAL_COMMON
+/* Keil::Device:STM32Cube HAL:Cortex:1.7.9 */
+#define RTE_DEVICE_HAL_CORTEX
+/* Keil::Device:STM32Cube HAL:DMA:1.7.9 */
+#define RTE_DEVICE_HAL_DMA
+/* Keil::Device:STM32Cube HAL:GPIO:1.7.9 */
+#define RTE_DEVICE_HAL_GPIO
+/* Keil::Device:STM32Cube HAL:PWR:1.7.9 */
+#define RTE_DEVICE_HAL_PWR
+/* Keil::Device:STM32Cube HAL:RCC:1.7.9 */
+#define RTE_DEVICE_HAL_RCC
+/* Keil::Device:STM32Cube HAL:SPI:1.7.9 */
+#define RTE_DEVICE_HAL_SPI
+/* Keil::Device:STM32Cube HAL:SRAM:1.7.9 */
+#define RTE_DEVICE_HAL_SRAM
+/* Keil::Device:STM32Cube HAL:TIM:1.7.9 */
+#define RTE_DEVICE_HAL_TIM
+/* Keil::Device:Startup:2.6.3 */
+#define RTE_DEVICE_STARTUP_STM32F4XX    /* Device Startup for STM32F4 */
+
+
+#endif /* RTE_COMPONENTS_H */
